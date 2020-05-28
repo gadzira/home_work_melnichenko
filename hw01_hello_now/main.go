0a80ccadfa9c0ef0941eeb7bf3d74a92fe9aa96c
emelnichenko <emelnichenko@odin.com>
2020-05-28 22:19:50 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
 	// Place your code here
+	const (
+		NtpServer = "0.beevik-ntp.pool.ntp.org"
+		FatalfMsg = "Панека, беда, егор, отложенные вызовы не будут вызваны, буферы не отчистятся, потому что: %s"
+	)
+
+	et, err := ntp.Time(NtpServer)
+	if err != nil {
+		log.Fatalf(FatalfMsg, err)
+	}
+	ct := time.Now().UTC()
+	fmt.Printf("current time: %s\n", ct.Format("2006-01-02 15:04:05 +0000 MST"))
+	fmt.Printf("exact time: %s\n", et.Format("2006-01-02 15:04:05 +0000 MST"))
 }
