b2722b47aeb6b6d21cbee07db231d55dbbdf181b
emelnichenko <emelnichenko@odin.com>
2020-06-11 11:50:07 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+// ErrInvalidString ...
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(s string) (string, error) {
+	// Check if a string is empty
+	if s == "" {
+		return "", nil
+	}
+	// Check if a first simbol not digit
+	if unicode.IsDigit([]rune(s)[0]) {
+		return "", ErrInvalidString
+	}
+
+	// Check if a string contains more than one number
+	twoOrMoreNumber := regexp.MustCompile(`[0-9]{2,}`)
+	if twoOrMoreNumber.MatchString(s) {
+		return "", ErrInvalidString
+	}
+
+	// Check if a string haven't some digis
+	lessOneNumber := regexp.MustCompile(`[0-9]{1,}`)
+	if !lessOneNumber.MatchString(s) {
+		return s, nil
+	}
+
+	var sb strings.Builder
+	for i, v := range s {
+		if unicode.IsDigit(v) {
+			sv, _ := strconv.Atoi(string(v))
+			sb.WriteString(strings.Repeat(string(s[i-1]), sv-1))
+		} else {
+			sb.WriteString(string(v))
+		}
+	}
+	return sb.String(), nil
 }
