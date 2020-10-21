489f793367c65a521ffd1f5fb9cd090adafdb6c6
Мельниченко Евгений Александрович <MelnichenkoEA@sibintek.ru>
2020-10-21 11:43:48 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	cmdExec := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	envList := []string{}
+	for key, value := range env {
+		if _, ok := os.LookupEnv(key); ok {
+			os.Unsetenv(key)
+		}
+		if strings.Contains(value, "=") {
+			continue
+		}
+		if value != "" {
+			envList = append(envList, fmt.Sprintf("%s=%s", key, value))
+		}
+	}
+	cmdExec.Env = append(os.Environ(), envList...)
+	cmdExec.Stdin = os.Stdin
+	cmdExec.Stdout = os.Stdout
+	cmdExec.Stderr = os.Stderr
+	err := cmdExec.Run()
+	var eerr *exec.Error
+	if errors.As(err, &eerr) {
+		fmt.Println(wrapError(ErrExecCommand))
+
+		return defaultErrCode
+	}
+
+	return cmdExec.ProcessState.ExitCode()
 }
